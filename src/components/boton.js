7ed21e0a89026b80fc5696f35e87ebcea7377134
francovillarruel <89492469+francovillarruel@users.noreply.github.com>
2023-06-05 23:26:43 -0300

    Create boton.js

---
+++ src/components/boton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../estilos/Boton.css';
+
+function  Boton({texto, esBotondeClick, manejarClick}){
+    return (
+      <button
+      className={ esBotondeClick ? 'boton-click' : 'boton-reiniciar' }
+      onClick={manejarClick}>
+      {texto}
+      </button>
+    );
+}
+export default Boton; 
